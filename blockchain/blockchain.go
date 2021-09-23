2e772b1c9c7807e7db3c2010a3d765203f1e7eb9
chanbak <chan157@naver.com>
2021-09-24 01:27:41 +0900

    Done Baseline of the BlockChain

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,13 +1,19 @@
 package blockchain
 
+import (
+	"crypto/sha256"
+	"fmt"
+	"sync"
+)
+
 type block struct {
-	data     string
-	hash     string
-	prevHash string
+	Data     string
+	Hash     string
+	PrevHash string
 }
 
 type blockChain struct {
-	blocks []block
+	blocks []*block
 }
 
 /* Singleton Pattern
@@ -18,9 +24,46 @@ There is only one instance in our application
 // Only inside of package we can access
 var b *blockChain
 
+// Initialize only once
+var once sync.Once
+
 func GetBlockChain() *blockChain {
 	if b == nil {
-		b = &blockChain{}
+		once.Do(func() {
+			b = &blockChain{}
+			b.AddBlock("Genesis Block")
+		})
 	}
 	return b
+}
+
+func createBlock(data string) *block {
+	newBlock := block{
+		Data:     data,
+		Hash:     "",
+		PrevHash: getLastHash(),
+	}
+	newBlock.calculateHash()
+	return &newBlock
+}
+
+func getLastHash() string {
+	totalBlocks := len(GetBlockChain().blocks)
+	if totalBlocks == 0 {
+		return ""
+	}
+	return GetBlockChain().blocks[totalBlocks-1].Hash
+}
+
+func (b *block) calculateHash() {
+	hash := sha256.Sum256([]byte(b.Data + b.PrevHash))
+	b.Hash = fmt.Sprintf("%x", hash)
+}
+
+func (b *blockChain) AddBlock(data string) {
+	b.blocks = append(b.blocks, createBlock(data))
+}
+
+func (b *blockChain) AllBlocks() []*block {
+	return b.blocks
 }
--- main.go
+++ main.go
@@ -1,36 +1,19 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/chan157/nomadcoin/blockchain"
 )
 
 func main() {
 	chain := blockchain.GetBlockChain()
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Thrid Block")
+	chain.AddBlock("Fourth Block")
+	for _, block := range chain.AllBlocks() {
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %s\n", block.Hash)
+		fmt.Printf("PrevHash: %s\n\n", block.PrevHash)
+	}
 }
-
-// func (b *blockChain) getLastHash() string {
-// 	if len(b.blocks) > 0 {
-// 		return b.blocks[len(b.blocks)-1].hash
-// 	}
-// 	return ""
-// }
-// func (b *blockChain) addBlock(data string) {
-// 	newBlock := block{
-// 		data:     data,
-// 		hash:     "",
-// 		prevHash: b.getLastHash(),
-// 	}
-
-// 	hash := sha256.Sum256([]byte(newBlock.data + newBlock.prevHash))
-// 	hexHash := fmt.Sprintf("%x", hash)
-// 	newBlock.hash = hexHash
-// 	b.blocks = append(b.blocks, newBlock)
-// }
-
-// func (b *blockChain) listBlocks() {
-// 	for _, block := range b.blocks {
-// 		fmt.Printf("Data : %s\n", block.data)
-// 		fmt.Printf("Hash : %s\n", block.hash)
-// 		fmt.Printf("PrevHash : %s\n\n", block.prevHash)
-// 	}
-// }
