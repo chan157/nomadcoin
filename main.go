5674633f482a811544f3e41f58b4440c78790bd4
chanbak <chan157@naver.com>
2021-09-24 00:54:18 +0900

    Add BlockChain

--- main.go
+++ main.go
@@ -6,19 +6,47 @@ import (
 )
 
 type block struct {
-	data    string
-	hash    string
-	preHash string
+	data     string
+	hash     string
+	prevHash string
+}
+
+type blockChain struct {
+	blocks []block
 }
 
 func main() {
-	genesisBlock := block{
-		data:    "Genesis Block",
-		hash:    "",
-		preHash: "",
+	chain := blockChain{}
+	chain.addBlock("Genesis Block")
+	chain.addBlock("Second Block")
+	chain.addBlock("Third Block")
+
+	chain.listBlocks()
+}
+
+func (b *blockChain) getLastHash() string {
+	if len(b.blocks) > 0 {
+		return b.blocks[len(b.blocks)-1].hash
 	}
-	hash := sha256.Sum256([]byte(genesisBlock.data + genesisBlock.preHash))
+	return ""
+}
+func (b *blockChain) addBlock(data string) {
+	newBlock := block{
+		data:     data,
+		hash:     "",
+		prevHash: b.getLastHash(),
+	}
+
+	hash := sha256.Sum256([]byte(newBlock.data + newBlock.prevHash))
 	hexHash := fmt.Sprintf("%x", hash)
-	genesisBlock.hash = hexHash
-	fmt.Println(genesisBlock)
+	newBlock.hash = hexHash
+	b.blocks = append(b.blocks, newBlock)
+}
+
+func (b *blockChain) listBlocks() {
+	for _, block := range b.blocks {
+		fmt.Printf("Data : %s\n", block.data)
+		fmt.Printf("Hash : %s\n", block.hash)
+		fmt.Printf("PrevHash : %s\n\n", block.prevHash)
+	}
 }
